918386ce2848e3683e12ec30cb52b733f167fb6b
link1345 <>
2025-05-31 16:09:46 +0900

    config修正

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,22 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
+const ignoreConfig = {
+  ignores: ["**/*.{js,mjs,cjs}"],
+};
+
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
+  ignoreConfig,
+  {
+    files: ["src/**/*.{ts,mts,cts}"],
+    languageOptions: {
+      globals: globals.browser, parserOptions: {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        parser: "@typescript-eslint/parser"
+      },
+    },
+  },
   tseslint.configs.recommended,
 ]);
